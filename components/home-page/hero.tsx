54870d3d9b7f66f91fa1b40e3f800521dbf21706
crazyoptimist <hey@crazyoptimist.net>
2024-09-25 15:47:26 -0500

    init: with next, tailwind, shadcn, brijr/craft

---
+++ components/home-page/hero.tsx
@@ -0,0 +1,38 @@
+// React and Next.js imports
+import Link from "next/link";
+import Image from "next/image";
+
+// Third-party library imports
+import Balancer from "react-wrap-balancer";
+import { Camera } from "lucide-react";
+
+// Local component imports
+import { Section, Container } from "@/components/craft";
+import { Button } from "@/components/ui/button";
+
+// Asset imports
+import Logo from "@/public/logo.svg";
+
+const Hero = () => {
+  return (
+    <Section>
+      <Container className="flex flex-col items-center text-center">
+        <Image
+          src={Logo}
+          width={172}
+          height={72}
+          alt="Company Logo"
+          className="not-prose mb-6 dark:invert md:mb-8"
+        />
+        <h1 className="!mb-0">
+          <Balancer>
+            Coming soon ...
+          </Balancer>
+        </h1>
+      </Container>
+    </Section>
+  );
+};
+
+export default Hero;
+
